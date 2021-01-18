4306390237f2a7a9b80d5dc408d390e4e0cc807a
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-18 09:05:18 -0300

    create banco de dados

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from flask import Flask
+from flask_restful import Api
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact_db.db'
+api = Api(app)
 
 
-# Press the green button in the gutter to run the script.
+@app.before_first_request
+def create_database():
+    db.create_all()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    from sql_alchemy import db
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    db.init_app(app)
+    app.run()
